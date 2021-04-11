c643ad171896d8ea9442813c4ad13cb6d59a1770
dwyerb <brendandwyer@hotmail.com>
2021-04-10 23:47:49 -0400

    Initial commit

---
+++ src/main/java/net/muroc/s5norrisja/S5norrisjaApplication.java
@@ -0,0 +1,13 @@
+package net.muroc.s5norrisja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5norrisjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S5norrisjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/muroc/s5norrisja/S5norrisjaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.muroc.s5norrisja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5norrisjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
